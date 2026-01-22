72c4a9474f069eb6d9a69640564384201ccab524
Vaishnavi Jaiswal <65708411+vaishnavi-88@users.noreply.github.com>
2026-01-22 14:57:45 +0100

    Add author comment in StartUp.java
    Added author comment to StartUp.java

--- token-service/src/main/java/dtu/fm22/token/StartUp.java
+++ token-service/src/main/java/dtu/fm22/token/StartUp.java
@@ -1,7 +1,9 @@
 package dtu.fm22.token;
 
 import messaging.implementations.RabbitMqQueue;
-
+/*
+ ** author s242576
+ */
 public class StartUp {
     public static void main(String[] args) throws Exception {
         new StartUp().startUp();
